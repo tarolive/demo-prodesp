589c6b019f98cc96637344e60e185cd70a73a745
Tarcisio <tarolive@redhat.com>
2024-10-23 18:51:59 +0000

    update

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from components.process_document import process_document
+from components.nlp              import nlp
+from components.process_document import get_subject_text, process_document
 from flask                       import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/', methods = ['POST'])
-def process_document() -> dict:
+def process_document() -> list:
 
     if 'file' not in request.files:
         return {
@@ -21,6 +22,16 @@ def process_document() -> dict:
 
     file.save(file.filename)
 
-    text = process_document(file.filename)
+    text     = process_document(file.filename)
+    subjects = []
 
-    return text
+    for page in text:
+
+        for subject in page['subjects']:
+
+            subject_text   = get_subject_text(text, page['index'], subject['index'])
+            subject['nlp'] = nlp(subject_text)
+
+            subjects.append(subject)
+
+    return subjects
